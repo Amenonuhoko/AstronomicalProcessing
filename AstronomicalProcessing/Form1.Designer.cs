ee64b9e0030c1528bad18d9466ec7f0501cf2375
P252069 <P252069@CDM508959>
2022-10-20 10:09:10 +0800

    Add project files.

---
+++ AstronomicalProcessing/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AstronomicalProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRandomAdd = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnBinarySearch = new System.Windows.Forms.Button();
+            this.btnBubbleSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 364);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(286, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnRandomAdd
+            // 
+            this.btnRandomAdd.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRandomAdd.Location = new System.Drawing.Point(317, 12);
+            this.btnRandomAdd.Name = "btnRandomAdd";
+            this.btnRandomAdd.Size = new System.Drawing.Size(146, 42);
+            this.btnRandomAdd.TabIndex = 2;
+            this.btnRandomAdd.Text = "Random Add";
+            this.btnRandomAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(286, 130);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 42);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEdit.Location = new System.Drawing.Point(411, 130);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(92, 42);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.TabStop = false;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnBinarySearch
+            // 
+            this.btnBinarySearch.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBinarySearch.Location = new System.Drawing.Point(340, 194);
+            this.btnBinarySearch.Name = "btnBinarySearch";
+            this.btnBinarySearch.Size = new System.Drawing.Size(107, 42);
+            this.btnBinarySearch.TabIndex = 5;
+            this.btnBinarySearch.Text = "Search";
+            this.btnBinarySearch.UseVisualStyleBackColor = true;
+            // 
+            // btnBubbleSort
+            // 
+            this.btnBubbleSort.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBubbleSort.Location = new System.Drawing.Point(340, 255);
+            this.btnBubbleSort.Name = "btnBubbleSort";
+            this.btnBubbleSort.Size = new System.Drawing.Size(107, 42);
+            this.btnBubbleSort.TabIndex = 6;
+            this.btnBubbleSort.Text = "Sort";
+            this.btnBubbleSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 426);
+            this.Controls.Add(this.btnBubbleSort);
+            this.Controls.Add(this.btnBinarySearch);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRandomAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button btnRandomAdd;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnBinarySearch;
+        private Button btnBubbleSort;
+    }
+}
